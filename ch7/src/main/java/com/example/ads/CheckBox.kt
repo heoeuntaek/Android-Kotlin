ed85158095439d3cccb57f9ea1e04b22b5a076f8
house_com <gjdmsxor@naver.com>
2022-12-10 19:49:02 +0900

    d

--- ch7/src/main/java/com/example/ads/CheckBox.kt
+++ ch7/src/main/java/com/example/ads/CheckBox.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_check_box.*
 
 class CheckBox : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_box)
@@ -18,17 +20,11 @@ class CheckBox : AppCompatActivity() {
         decafCheckBox.text = coffeeArr[2]
 
 
-        var sb = StringBuilder()
-        if (americanoCheckBox.isChecked) sb.append(" ${americanoCheckBox.text} ")
-        if (latteCheckBox.isChecked) sb.append(" ${latteCheckBox.text} ")
-        if (decafCheckBox.isChecked) sb.append(" ${decafCheckBox.text} ")
-        sb.append(" are ordered. Thanks!")
+        var rdArray: Array<android.widget.CheckBox> =
+            arrayOf(americanoCheckBox, latteCheckBox, decafCheckBox)
 
-        Toast.makeText(
-            applicationContext,
-            sb.toString(), Toast.LENGTH_LONG
-        ).show()
-
+        var nothingSelected = true
+        var case: Boolean = false
 
         selectButton.setOnClickListener {
             var sb = StringBuilder()
@@ -36,19 +32,41 @@ class CheckBox : AppCompatActivity() {
             if (americanoCheckBox.isChecked) {
                 sb.append(americanoCheckBox.text)
                 sb.append(" ${editAme.text.toString()} 잔 주문 \n")
-
+                nothingSelected = false
             }
 
             if (latteCheckBox.isChecked) {
                 sb.append(latteCheckBox.text)
                 sb.append(" ${editLat.text.toString()} 잔 주문 \n")
+                nothingSelected = false
             }
             if (decafCheckBox.isChecked) {
                 sb.append(decafCheckBox.text)
                 sb.append(" ${editDec.text.toString()} 잔 주문 \n")
+                nothingSelected = false
             }
 
-            Toast.makeText(applicationContext, sb.toString(), Toast.LENGTH_SHORT).show()
+
+            // 메뉴 선택안했는데 수량은 1보다 큰 경우
+
+
+
+            if (!americanoCheckBox.isChecked && editAme != null) case = true
+            if (!latteCheckBox.isChecked && editLat != null) case = true
+            if (!decafCheckBox.isChecked && editDec != null) case = true
+
+
+            if (nothingSelected) {
+                Toast.makeText(applicationContext, "아무것도 선택x", Toast.LENGTH_SHORT).show()
+            }
+
+            if (case) {
+                Toast.makeText(applicationContext, "메뉴를 선택 안했습니다", Toast.LENGTH_SHORT).show()
+            }
+
+
+            if (!nothingSelected && !case)
+                Toast.makeText(applicationContext, sb.toString(), Toast.LENGTH_SHORT).show()
 
 
         }
